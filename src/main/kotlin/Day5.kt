60ea8e2b334ff0586312d6442a519f5aee0208d3
Siddharth <siddharth@finzy.com>
2022-12-05 11:32:40 +0530

    day 5

---
+++ src/main/kotlin/Day5.kt
@@ -0,0 +1,65 @@
+import java.util.Stack
+
+fun main() {
+
+    val input = getText("day5/input2.txt").split("\n\n")
+    val stacks = stacks(input[0].lines().reversed())
+    val moves = moves(input[1].lines())
+
+    //part1(moves, stacks)
+    part2(moves, stacks)
+
+    val output = stacks.map { it.peek() }.joinToString("")
+
+    println(output)
+}
+
+private fun stacks(stackTxt: List<String>): List<Stack<Char>> {
+    val stackCount = stackTxt.first().split(" ").last().toInt()
+    val stacks = List(stackCount) { Stack<Char>() }
+
+    stackTxt.drop(1).map { line ->
+        line.chunked(4).mapIndexed { idx, it ->
+            if (it.isNotBlank())
+                stacks[idx].push(it[1])
+        }
+    }
+    return stacks
+}
+
+private fun moves(movesText: List<String>): List<Move> {
+    val regex = Regex("move (\\d+) from (\\d+) to (\\d+)")
+    val moves = movesText.map {
+        val values = regex.find(it)?.groupValues ?: error("invalid text")
+        Move(values[1].toInt(), values[2].toInt(), values[3].toInt())
+    }
+    return moves
+}
+
+private fun part1(
+    moves: List<Move>,
+    stacks: List<Stack<Char>>
+) {
+    moves.map { move ->
+        repeat(move.count) {
+            val item = stacks[move.from - 1].pop()
+            stacks[move.to - 1].push(item)
+        }
+    }
+}
+
+
+private fun part2(
+    moves: List<Move>,
+    stacks: List<Stack<Char>>
+) {
+    moves.map { move ->
+        (0 until move.count).map {
+            stacks[move.from - 1].pop()
+        }.reversed().forEach {
+            stacks[move.to - 1].push(it)
+        }
+    }
+}
+
+data class Move(val count: Int, val from: Int, val to: Int)
--- src/main/kotlin/util.kt
+++ src/main/kotlin/util.kt
@@ -4,6 +4,9 @@ import java.security.MessageDigest
 fun getLines(path: String): List<String> =
     object {}.javaClass.getResource(path)?.readText()?.lines() ?: emptyList()
 
+fun getText(path: String): String =
+    object {}.javaClass.getResource(path)?.readText() ?: ""
+
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
