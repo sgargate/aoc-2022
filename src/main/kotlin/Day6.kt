10190f30335b60b5924a5a4b9875c4f519a7fec9
Siddharth <siddharth@finzy.com>
2022-12-07 08:13:44 +0530

    got to know about windowed

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -1,26 +1,18 @@
-import java.util.concurrent.ArrayBlockingQueue
-
-private const val maxLength = 14
 
 fun main() {
     val inputs = getLines("day6/input1.txt")
 
     inputs.forEach {
-        println(startMarker(it)+1)
+        println(startMarker(it))
     }
 }
 
 private fun startMarker(input: String): Int {
-    val q = ArrayBlockingQueue<Char>(maxLength)
+    val maxLength = 14
 
-    input.forEachIndexed { i, c ->
-        q.offerAndReplace(c)
-        if (q.distinct().size == maxLength) return i
-    }
-    return -1
+    return input.windowed(maxLength).indexOfFirst {
+        it.toSet().size == maxLength
+    } + maxLength
+
 }
 
-fun ArrayBlockingQueue<Char>.offerAndReplace(c: Char) {
-    if(size == maxLength) take()
-    offer(c)
-}
